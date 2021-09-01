66533912a9e44e1c09e961745fd2d68e9c5175a4
himanshsoni <himanshsoni5@gmail.com>
2021-09-01 15:10:16 +0530

    file added

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo{
+	public static void main(String[] args) {
+		int fact=1;
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println("Factorial:"+fact);
+	}
+}
